15ea2a2d5495f98c3365b505666d5b4b41967ae8
haaalieee <morenoanalie14@gmail.com>
2023-05-18 17:20:49 +0800

    heroku fix

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,16 @@
 module.exports = () => ({
   "netlify-deployments": {
     enabled: true,
+    config: {
+      accessToken: "ST0bg0c4b4Dk_W0xIetFE-I7hMUZsqa6trGdIMIhSwA",
+      sites: [
+        {
+          name: 'cms-web-ar',
+          id: "083adf6c-a9e9-4297-8d5b-6432711e586c",
+          buildHook: "https://api.netlify.com/build_hooks/6465d254e833ba4e40b80459",
+          branch: 'master' // optional
+        }
+      ]
+    },
   },
 });
