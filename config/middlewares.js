e88d28e301ba1bb9a31933162d62a367738ab875
haaalieee <morenoanalie14@gmail.com>
2023-05-24 14:22:59 +0800

    Remove from heroku

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,16 +1,4 @@
 module.exports = [
-    'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
-          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('S3_BUCKET_NAME')}.s3.${env('AWS_REGION')}.amazonaws.com`],
-        },
-      }
-    },
-  },
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
