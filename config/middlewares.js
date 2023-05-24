dd788e849be483ca8aaa767837080d33edbc20aa
haaalieee <morenoanalie14@gmail.com>
2023-05-24 12:32:24 +0800

    Add config to host images

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,17 @@
 module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('MY_HEROKU_URL')}`],
+        },
+      }
+    },
+  },
+  'strapi::errors',
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
